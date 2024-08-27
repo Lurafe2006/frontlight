c227fff2cb11cd51162fc8fe12911457a32e65b1
Lurafe <lu.rafe2006@gmail.com>
2024-08-27 15:05:09 -0300

    subida de código

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { Header } from "@/sections/Header";
+import { Hero } from "@/sections/hero";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
+import { Testimonials } from "@/sections/testimonials";
+import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <LogoTicker />
+      <ProductShowcase />
+      <Pricing />
+      <Testimonials />
+      <CallToAction />
+      <Footer/>
+    </>
+  );
+}
